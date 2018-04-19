1e90cff6c050f0794055e6c896b2ec2da9f85005
Carlos Ernesto Lopez Nataren <carlos.nataren@gmail.com>
2018-04-18 19:00:51 -0500

    Changed some things in kernel.cu so the kernel modifies an image

--- dist_2d/kernel.cu
+++ dist_2d/kernel.cu
@@ -1,14 +1,51 @@
-__global__ void kernel2D( uchar4 *d_output, int w, int h, ... ) { 
-// Compute column and row indices. 
-int c = blockIdx.x* blockDim.x + threadIdx.x; 
-int r = blockIdx.y* blockDim.y + threadIdx.y; 
-int i = r * w + c; // 1D flat index 
-// Check if within image bounds. 
- if (( r > = h) | | (c > = w)) { 
- return; 
- } 
-d_output[ i].x = RED_FORMULA; // Compute red 
-d_output[ i]. y = GREEN_ FORMULA; // Compute green 
-d_output[ i]. z = BLUE_ FORMULA; // Compute blue 
-d_output[ i]. w = 255; // Fully opaque 
+#define W 500
+#define H 500
+#define TX 32
+#define TY 32
+
+__device__ unsigned char clip(int n){
+  return n > 255 ? 255 : (n < 0 ? 0 : n);
 }
+
+
+__global__ void distanceKernel(uchar4 *d_out, int w, int h, int2 pos){
+
+  const int c = blockIdx.x * blockDim.x + threadIdx.x;
+  const int c = blockIdx.y * blockDim.y + threadIdx.y;
+  const int i = r * w + c;
+
+  if ((c >= w) || (r >= h)) return;
+
+  //Compute the distance and set d_out[i]
+  d_out[i] = sqrtf((c - pos.x) * (c - pos.x) +
+		   (r - pos.y) * (r - pos.y));
+
+  // Convert distance to intensity value on interval [0, 255]
+  const unsigned char intensity = clip(255 - d);
+
+  d_out[i].x = intensity;
+  d_out[i].y = intensity;
+  d_out[i].z = 0;
+  d_out[i].z = 255; //fully opaque
+}
+
+int main()
+{
+  uchar4 *out = (uchar4) calloc(W*H, sizeof(uchar4));
+  uchar4 *d_out;
+  cudaMalloc(&d_out, W*H*sizeof(uchar4));
+  
+  const int2 pos = {0, 0};
+  const dim3 blockSize(TX, TY);
+  const int bx = (W + TX - 1) / TX;
+  const int bx = (W + TY - 1) / TY;
+  const dim3 gridSize = dim3(bx, by);
+
+  distanceKernel<<<gridSize, blockSize>>> (d_out, W, H, pos);
+
+  // Copy results to host
+
+  // Free Memory
+
+}
+  
